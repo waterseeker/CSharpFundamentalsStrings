ec2e76d5cbceabbdab9edf50f205965997c88e2e
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-08 12:59:27 -0500

    added example of a new line char inside of a string

--- CSharpFundamentalsStrings/Program.cs
+++ CSharpFundamentalsStrings/Program.cs
@@ -7,7 +7,8 @@ namespace CSharpFundamentalsStrings
         static void Main(string[] args)
         {
             //string myString = "Go to your c:\\ drive";
-            string myString = "My \"SortedDictionary called\" life";
+            //string myString = "My \"SortedDictionary called\" life";
+            string myString = "What if I need \n a new line?";
 
             Console.WriteLine(myString);
             Console.ReadLine();
