ca64d47f7bb74d7d711707038b0107148261d9dd
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-08 12:57:59 -0500

    added an example using backslashes to get around problem with using double quotes inside a string

--- CSharpFundamentalsStrings/Program.cs
+++ CSharpFundamentalsStrings/Program.cs
@@ -6,7 +6,8 @@ namespace CSharpFundamentalsStrings
     {
         static void Main(string[] args)
         {
-            string myString = "Go to your c:\\ drive";
+            //string myString = "Go to your c:\\ drive";
+            string myString = "My \"SortedDictionary called\" life";
 
             Console.WriteLine(myString);
             Console.ReadLine();
